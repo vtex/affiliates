ffa69a5bb57373f3141072660c0925d797dbc9a5
GabrielEluan <gabrieleluan9@gmail.com>
2021-12-14 11:30:00 -0300

    Add clientAffiliation Master Data Client

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,12 +1,19 @@
 import { IOClients } from '@vtex/api'
 import { masterDataFor, Checkout, OMS } from '@vtex/clients'
-import type { Affiliates } from 'vtex.affiliates'
+import type { Affiliates, ClientAffiliation } from 'vtex.affiliates'
 
 import AuthenticationClient from './authenticationClient'
 
 export class Clients extends IOClients {
   public get affiliates() {
     return this.getOrSet('affiliates', masterDataFor<Affiliates>('affiliates'))
+  }
+
+  public get clientAffiliation() {
+    return this.getOrSet(
+      'clientAffiliation',
+      masterDataFor<ClientAffiliation>('clientAffiliation')
+    )
   }
 
   public get checkout() {
