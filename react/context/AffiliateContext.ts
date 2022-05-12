c816199f3944ddddbc8278c49a0fb0f7135a4193
Matheus Izidio <matheusimeira@gmail.com>
2022-05-12 10:35:51 -0300

    adding orders to the context and saving affiliate data to the context

--- react/context/AffiliateContext.ts
+++ react/context/AffiliateContext.ts
@@ -1,13 +1,23 @@
 import React from 'react'
 
+import type { AffiliatesOrdersData, Totalizers } from '../typings/tables'
+
 interface AffiliateContextType {
   isValid: boolean
   isLogged: boolean
+  refetchAffiliate: () => void
+  refetchOrders: () => void
+  affiliate: unknown
+  orders?: AffiliatesOrdersData[]
+  totalizer?: Totalizers
 }
 
 const AffiliateContext = React.createContext<AffiliateContextType>({
   isValid: false,
   isLogged: false,
+  refetchAffiliate: () => {},
+  refetchOrders: () => {},
+  affiliate: {},
 })
 
 export default AffiliateContext
