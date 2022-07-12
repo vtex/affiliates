f24b9e7897d1c0bbaeca472cb04818708cb99af9
GabrielBarros <gabriel.barros@vtex.com.br>
2022-07-12 08:24:14 -0300

    Base Logo added

--- react/AffiliateProfileTopBar.tsx
+++ react/AffiliateProfileTopBar.tsx
@@ -6,13 +6,13 @@ import { useIntl } from 'react-intl'
 import Avatar from './components/store/affiliateTopBar/Avatar/Avatar'
 import useAffiliate from './context/useAffiliate'
 import { storeMessages } from './utils/messages'
-//import LogoImage from './images/logo_rebel.png'
 
 interface Props {
   imageURL: string
 }
 
-const logoPadrao = "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/VTEX_Logo.svg/1200px-VTEX_Logo.svg.png"
+const baseLogo =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/VTEX_Logo.svg/1200px-VTEX_Logo.svg.png'
 
 const AffiliateTopBar = (props: Props) => {
   const { imageURL } = props
@@ -46,9 +46,9 @@ const AffiliateTopBar = (props: Props) => {
       <div className="flex pa4 justify-between">
         <div className="flex items-center-s">
           <img
-            src={imageURL || logoPadrao}
+            src={imageURL || baseLogo}
             alt={`${account} Logo`}
-            style={{ maxHeight: '50px' }}
+            style={{ maxHeight: '35px', paddingRight: '10px' }}
           />
           <h6 className="mb0">{`${intl.formatMessage(
             storeMessages.affiliateProfileTitle
@@ -82,7 +82,7 @@ AffiliateTopBar.schema = {
       title: 'Logo Image',
       description: 'Imagem que será disponibilizada como logo na página',
       type: 'string',
-      default: LogoImage,
+      default: baseLogo,
       widget: {
         'ui:widget': 'image-uploader',
       },
