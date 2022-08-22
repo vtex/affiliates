9fa8b6bc1749dc27dbf1e5ade59246f0df7924c7
GabrielBarros <gabriel.barros@vtex.com.br>
2022-08-22 12:32:03 -0300

    Utils adjustments

--- react/utils/constants.ts
+++ react/utils/constants.ts
@@ -1,6 +1,6 @@
 export const PAGE_SIZE = 15
 export const INITIAL_PAGE = 1
-export const MAX_PAGE_SIZE = 100
+export const MAX_PAGE_SIZE = 5
 export const MIN_TABLE_SIZE = 5
 
 // Our component treats 0 as infinity
