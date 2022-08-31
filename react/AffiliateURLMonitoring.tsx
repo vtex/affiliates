120809bb9dee73a384f35c8a4830abce8aaa2218
GabrielBarros <gabriel.barros@vtex.com.br>
2022-08-31 17:51:24 -0300

    URL monitoring block added within its core logic

---
+++ react/AffiliateURLMonitoring.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useMutation } from 'react-apollo'
+import { useOrderForm } from 'vtex.order-manager/OrderForm'
+import { useRuntime } from 'vtex.render-runtime'
+
+import SET_ON_ORDER_FORM_MUTATION from './graphql/setAffiliateOnOrderForm.graphql'
+import { DEFAULT_ORDER_FORM_ID } from './utils/constants'
+
+function AffiliateURLMonitoring() {
+  const { orderForm, setOrderForm } = useOrderForm()
+  const { route } = useRuntime()
+
+  const slug = route?.queryString?.targeting
+  const orderFormId = orderForm.id
+
+  // eslint-disable-next-line no-console
+  console.log(orderForm)
+
+  const [setAffiliateOnOrderForm, { called: mutationHasBeenCalled }] =
+    useMutation(SET_ON_ORDER_FORM_MUTATION, {
+      onCompleted: (data) => setOrderForm(data.setAffiliateOnOrderForm),
+    })
+
+  if (
+    !mutationHasBeenCalled &&
+    orderFormId !== DEFAULT_ORDER_FORM_ID &&
+    orderForm?.customData === null &&
+    slug
+  ) {
+    setAffiliateOnOrderForm({
+      variables: { orderFormId, slug },
+    })
+  }
+
+  return <></>
+}
+
+export default AffiliateURLMonitoring
--- react/utils/constants.ts
+++ react/utils/constants.ts
@@ -6,3 +6,5 @@ export const MIN_TABLE_SIZE = 5
 // Our component treats 0 as infinity
 export const COMMISSIONS_TABLE_EXPORT_LIMIT = 0
 export const AFFILIATES_ORDERS_EXPORT_LIMIT = 15000
+
+export const DEFAULT_ORDER_FORM_ID = 'default-order-form'
